1094ca7588a85b8682d14c9a1f8bbd9ad752b548
UrUrZa <IshvarIndrovich@mail.ru>
2023-12-21 18:17:10 +0400

    use MyDndComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Example } from './components/Example';
+import { Example, MyDndComponent } from './components/Example';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Example/>
+        <MyDndComponent/>
       </header>
     </div>
   );

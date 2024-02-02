d364b51253283b021f4ff8044c98ec71ea485f81
UrUrZa <IshvarIndrovich@mail.ru>
2024-02-02 14:00:15 +0400

    add more stories

--- src/components/DraggableRectangle/DraggableRectangle.stories.tsx
+++ src/components/DraggableRectangle/DraggableRectangle.stories.tsx
@@ -11,25 +11,7 @@ const componentDefaultProps: IDraggableRectangleProps = {
     onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
     title: "DraggableRectangle",
     color: "red",
-    cardId: "1",
-    dropZone: {
-        onAcceptDrop: () => console.log("HorizontalListDropZone onAcceptDrop"),
-        isAcceptableDrop: true,
-        dropZoneContent: [{
-            onDragStart: () => console.log("DraggableRectangle onDragStart"),
-            onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
-            title: "DraggableRectangle",
-            color: "red",
-            cardId: "1",
-            dropZone: {
-                onAcceptDrop: () => console.log("HorizontalListDropZone onAcceptDrop"),
-                isAcceptableDrop: true,
-                dropZoneContent: [],
-                isAcceptableNew: true,
-            } 
-        }],
-        isAcceptableNew: true,
-    } 
+    cardId: "1", 
 }
 
 export const DraggableRectangleStory = () => {
@@ -42,3 +24,243 @@ export const DraggableRectangleStory = () => {
         </>
     );
 }
+
+export const DraggableRectangleWithOnClearStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        onClear: () => console.log("DraggableRectangle onClear"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithOnDrillDownClickStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        onDrillDownClick: () => console.log("DraggableRectangle onDrillDownClick"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithBothOnClearAndOnDrillDownClickStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        onClear: () => console.log("DraggableRectangle onClear"),
+        onDrillDownClick: () => console.log("DraggableRectangle onDrillDownClick"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [],
+            isAcceptableNew: true,
+        },
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneAndOnClearStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [],
+            isAcceptableNew: true,
+        },
+        onClear: () => console.log("DraggableRectangle onClear"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneAndOnDrillDownClickStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [],
+            isAcceptableNew: true,
+        },
+        onDrillDownClick: () => console.log("DraggableRectangle onDrillDownClick"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneAndBothOnClearAndOnDrillDownClickStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [],
+            isAcceptableNew: true,
+        },
+        onClear: () => console.log("DraggableRectangle onClear"),
+        onDrillDownClick: () => console.log("DraggableRectangle onDrillDownClick"),
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneWithContentStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [
+                {
+                    onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                    onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                    title: "DraggableRectangle",
+                    color: "red",
+                    cardId: "1",
+                    onClear: () => console.log("DraggableRectangle onClear"),
+                }
+            ],
+            isAcceptableNew: true,
+        },
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneWithTwoContentStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [
+                {
+                    onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                    onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                    title: "DraggableRectangle",
+                    color: "red",
+                    cardId: "1",
+                    onClear: () => console.log("DraggableRectangle onClear"),
+                },
+                {
+                    onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                    onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                    title: "DraggableRectangle",
+                    color: "red",
+                    cardId: "2",
+                    onClear: () => console.log("DraggableRectangle onClear"),
+                }
+            ],
+            isAcceptableNew: true,
+        },
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
+export const DraggableRectangleWithDropZoneWithContentDropZoneStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [
+                {
+                    onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                    onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                    title: "DraggableRectangle",
+                    color: "red",
+                    cardId: "1",
+                    onClear: () => console.log("DraggableRectangle onClear"),
+                    dropZone: {
+                        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+                        isAcceptableDrop: true,
+                        dropZoneContent: [],
+                        isAcceptableNew: true,
+                    },
+                }
+            ],
+            isAcceptableNew: true,
+        },
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+export const DraggableRectangleWithDropZoneWithContentDropZoneWithContentStory = () => {
+    const componentProps: IDraggableRectangleProps = {
+        dropZone: {
+            onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+            isAcceptableDrop: true,
+            dropZoneContent: [
+                {
+                    onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                    onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                    title: "DraggableRectangle",
+                    color: "red",
+                    cardId: "1",
+                    onClear: () => console.log("DraggableRectangle onClear"),
+                    dropZone: {
+                        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+                        isAcceptableDrop: true,
+                        dropZoneContent: [{
+                            onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                            onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                            title: "DraggableRectangle",
+                            color: "red",
+                            cardId: "2",
+                            onClear: () => console.log("DraggableRectangle onClear"),
+                        }],
+                        isAcceptableNew: true,
+                    },
+                }
+            ],
+            isAcceptableNew: true,
+        },
+        ...componentDefaultProps,
+    }
+    return (
+        <>
+            <DraggableRectangle {...componentProps}/>
+        </>
+    );
+}
+
--- src/components/HorizontalListDropZone/HorizontalListDropZone.stories.tsx
+++ src/components/HorizontalListDropZone/HorizontalListDropZone.stories.tsx
@@ -8,26 +8,157 @@ export default {
 
 const componentDefaultProps: IHorizontalListDropZoneProps = {
     onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
-    isAcceptableDrop: true,
-    dropZoneContent: [{
-        onDragStart: () => console.log("DraggableRectangle onDragStart"),
-        onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
-        title: "DraggableRectangle",
-        color: "red",
-        cardId: "1",
-        dropZone: {
-            onAcceptDrop: () => console.log("HorizontalListDropZone onAcceptDrop"),
-            isAcceptableDrop: true,
-            dropZoneContent: [],
-            isAcceptableNew: true,
-        } 
-    }],
-    isAcceptableNew: true,
+    isAcceptableDrop: false,
+    dropZoneContent: [],
+    isAcceptableNew: false,
 }
 
-export const DraggableRectangleStory = () => {
+export const HorizontalListDropZoneStory = () => {
     const componentProps: IHorizontalListDropZoneProps = {
         ...componentDefaultProps,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithAcceptableDropStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: true,
+        dropZoneContent: [],
+        isAcceptableNew: false,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithAcceptableNewStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: false,
+        dropZoneContent: [],
+        isAcceptableNew: true,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithContentStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: true,
+        dropZoneContent: [
+            {
+                onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                title: "DraggableRectangle",
+                color: "red",
+                cardId: "cardId",
+            }
+        ],
+        isAcceptableNew: true,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithContentDropZoneStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: true,
+        dropZoneContent: [
+            {
+                onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                title: "DraggableRectangle",
+                color: "red",
+                cardId: "cardId",
+                dropZone: {
+                    onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+                    isAcceptableDrop: false,
+                    dropZoneContent: [],
+                    isAcceptableNew: false,
+                },
+                onClear: () => console.log("DraggableRectangle onClear"),
+            }
+        ],
+        isAcceptableNew: true,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithContentDropZoneAcceptableStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: true,
+        dropZoneContent: [
+            {
+                onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                title: "DraggableRectangle",
+                color: "red",
+                cardId: "cardId",
+                dropZone: {
+                    onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+                    isAcceptableDrop: true,
+                    dropZoneContent: [],
+                    isAcceptableNew: true,
+                },
+                onClear: () => console.log("DraggableRectangle onClear"),
+            }
+        ],
+        isAcceptableNew: true,
+    }
+    return (
+        <>
+            <HorizontalListDropZone {...componentProps}/>
+        </>
+    );
+}
+
+export const HorizontalListDropZoneWithContentDropZoneAcceptableNewStory = () => {
+    const componentProps: IHorizontalListDropZoneProps = {
+        onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+        isAcceptableDrop: true,
+        dropZoneContent: [
+            {
+                onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                title: "DraggableRectangle",
+                color: "red",
+                cardId: "cardId",
+                dropZone: {
+                    onAcceptDrop: () => console.log("HorizontalListDropZone clicked"),
+                    isAcceptableDrop: true,
+                    dropZoneContent: [{
+                        onDragStart: () => console.log("DraggableRectangle onDragStart"),
+                        onDragEnd: () => console.log("DraggableRectangle onDragEnd"),
+                        title: "DraggableRectangle",
+                        color: "red",
+                        cardId: "cardId",
+                        onClear: () => console.log("DraggableRectangle onClear"),
+                    }],
+                    isAcceptableNew: false,
+                },
+            }
+        ],
+        isAcceptableNew: true,
     }
     return (
         <>

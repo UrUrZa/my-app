83ad595d78654e2da19b6edb3e98ad2215bd8e8f
UrUrZa <IshvarIndrovich@mail.ru>
2024-02-05 11:01:35 +0400

    Add drag and drop functionality to HorizontalListDropZone component

--- src/components/HorizontalListDropZone/index.tsx
+++ src/components/HorizontalListDropZone/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useDroppable } from '@dnd-kit/core';
+import { DndContext, useDroppable, useSensor, useSensors, PointerSensor, DragEndEvent } from '@dnd-kit/core';
 import { Box, Paper, Typography } from '@mui/material';
 import { DraggableRectangle, IDraggableRectangleProps } from '../DraggableRectangle';
+import styled from '@emotion/styled';
 
 export interface IHorizontalListDropZoneProps {
   onAcceptDrop: () => void;
@@ -18,20 +19,52 @@ export const HorizontalListDropZone: React.FC<IHorizontalListDropZoneProps> = ({
 }) => {
   const { setNodeRef } = useDroppable({
     id: 'dropzone',
+    disabled: isAcceptableDrop!,
   });
 
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      // Настройки для PointerSensor
+    })
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { over } = event;
+    if (over && over.id === 'dropzone') {
+      onAcceptDrop();
+      console.log(`HorizontalListDropZone onAcceptDrop: ${over.id}`);
+    }
+  };
+
+  const StyledBox = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    `;
+
+  const StyledBoxDropHere = styled(Box)`
+    margin-top: 2;
+    padding: 2;
+    border: 2px dashed #999;
+    border-radius: 4px;
+    cursor: pointer;
+    `;
+
   return (
-    <Paper ref={setNodeRef} elevation={3} sx={{ p: 2, backgroundColor: isAcceptableDrop ? '#b2fab4' : 'grey' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        {dropZoneContent.map((content, index) => (
-          <DraggableRectangle key={index} {...content} />
-        ))}
-        {isAcceptableNew && (
-          <Box sx={{ mt: 2, p: 2, border: '2px dashed #999', borderRadius: '4px', cursor: 'pointer' }} onClick={onAcceptDrop}>
-            <Typography color="#999">Drop here</Typography>
-          </Box>
-        )}
-      </Box>
-    </Paper>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+      <Paper ref={setNodeRef} elevation={3} sx={{ p: 2, backgroundColor: isAcceptableDrop ? '#b2fab4' : 'grey' }}>
+        <StyledBox>
+          {dropZoneContent.map((content, index) => (
+            <DraggableRectangle key={index} {...content} />
+          ))}
+          {isAcceptableNew && (
+            <StyledBoxDropHere>
+              <Typography color="#999">Drop here</Typography>
+            </StyledBoxDropHere>
+          )}
+        </StyledBox>
+      </Paper>
+    </DndContext>
   );
 };
